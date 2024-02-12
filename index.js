8ad6c140e3e25d1d824e5c7b06f5d792a11b77f1
tarafdar16 <imrantarafdar16@gmail.com>
2024-02-12 17:06:46 +0600

    done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 require("dotenv").config();
-// const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -34,10 +34,245 @@ async function run() {
     const cartCollection = client.db("Bboss").collection("carts");
     const paymentCollection = client.db("Bboss").collection("payments");
 
-    app.get("/menu", async (req, res) => {
-      const result = await menuCollection.find().toArray();
-      res.send(result);
+   //  jwt token
+   app.post("/jwt", (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "1h",
     });
+    res.send({ token });
+  });
+
+  // verifyToken middleware ================
+
+  const verifyToken = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    // bearer token ==============
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res
+          .status(401)
+          .send({ error: true, message: "unauthorized access" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  };
+
+  // warning:use verifyJWT before using verifyAdmin
+  const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    if (user?.role !== "admin") {
+      return res
+        .status(403)
+        .send({ error: true, message: "Forbidden message" });
+    }
+    next();
+  };
+
+  /**
+   * 0.do not show secure links to those who should not see the links
+   * use jwt token : verifyToken
+   * use verifyAdmin middleware
+   *
+   */
+  // user related api
+  app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+    const result = await usersCollection.find().toArray();
+    res.send(result);
+  });
+
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email };
+    const existingUser = await usersCollection.findOne(query);
+    console.log("existingUser user", existingUser);
+    if (existingUser) {
+      return res.send({ message: "user already exists" });
+    }
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
+
+  // security layer : verifyToken
+  // email same
+  // check admin
+
+  app.get("/users/admin/:email", verifyToken, async (req, res) => {
+    const email = req.params.email;
+
+    if (req.decoded.email !== email) {
+      res.send({ admin: false });
+    }
+
+    const query = { email: email };
+    const user = await usersCollection.findOne(query);
+    const result = { admin: user?.role === "admin" };
+    res.send(result);
+  });
+
+  app.patch("/users/admin/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        role: "admin",
+      },
+    };
+    const result = await usersCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+
+  // menu related api
+  app.get("/menu", async (req, res) => {
+    const result = await menuCollection.find().toArray();
+    res.send(result);
+  });
+
+  app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+    const newItem = req.body;
+    const result = await menuCollection.insertOne(newItem);
+    res.send(result);
+  });
+
+  app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await menuCollection.deleteOne(query);
+    res.send(result);
+  });
+
+  // review related api
+  app.get("/reviews", async (req, res) => {
+    const result = await reviewsCollection.find().toArray();
+    res.send(result);
+  });
+
+  // cart collections
+
+  app.get("/carts", verifyToken, async (req, res) => {
+    const email = req.query.email;
+    if (!email) {
+      res.send([]);
+    }
+    const decodedEmail = req.decoded.email;
+    if (email !== decodedEmail) {
+      return res
+        .status(403)
+        .send({ error: true, message: "Forbidden access" });
+    }
+    const query = { email: email };
+    const result = await cartCollection.find(query).toArray();
+    res.send(result);
+  });
+
+  app.post("/carts", async (req, res) => {
+    const item = req.body;
+    console.log(item);
+    const result = await cartCollection.insertOne(item);
+    res.send(result);
+  });
+
+  app.delete("/carts/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  });
+
+  // create payment intent
+
+  app.post("/create-payment-intent", verifyToken, async (req, res) => {
+    const { price } = req.body;
+    const amount = parseInt(price * 100);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  });
+
+  // payment related api ===============
+  app.post("/payment", verifyToken, async (req, res) => {
+    const payment = req.body;
+    const insertResult = await paymentCollection.insertOne(payment);
+    const query = {
+      _id: { $in: payment.cartItems.map((id) => new ObjectId(id)) },
+    };
+    const deleteResult = await cartCollection.deleteMany(query);
+    res.send({ insertResult, deleteResult });
+  });
+
+  app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+    const users = await usersCollection.estimatedDocumentCount();
+    const products = await menuCollection.estimatedDocumentCount();
+    const orders = await paymentCollection.estimatedDocumentCount();
+
+    // bet way to get sum of the a field is to use group and sum operator
+    const payments = await paymentCollection.find().toArray();
+    const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
+    res.send({
+      revenue,
+      users,
+      products,
+      orders,
+    });
+  });
+
+  /**
+   * ---------------------
+   * bangla system (second best solution)
+   *
+   * 1.load all payments
+   * 2.for each payment,get the menuItems array
+   * 3.for each item in the menuItems array get the menuitem from the menu collection
+   * 4.put them in an array:allOrderItems
+   * 5.separate allOrderedItems by category using filter
+   * 6.now get the quantity by using length: pizza.length
+   * 7.for each category use recude to get the total amount spent on this category
+   *
+   */
+  app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
+    const pipeline = [
+      {
+        $lookup: {
+          from: "menu",
+          localField: "menuItems",
+          foreignField: "_id",
+          as: "menuItemsData",
+        },
+      },
+      { $unwind: "$menuItemsData" },
+      {
+        $group: {
+          _id: "$menuItemsData.category",
+          count: { $sum: 1 },
+          totalPrice: { $sum: "$menuItemsData.price" },
+        },
+      },
+      {
+        $project: {
+          category: "$_id",
+          count: "1",
+          total: { $round: ["$total", 2] },
+          _id: 0,
+        },
+      },
+    ];
+    const result = await paymentCollection.aggregate(pipeline).toArray();
+    res.send(result);
+  });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
